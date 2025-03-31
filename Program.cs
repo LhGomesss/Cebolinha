a2d72c27c7aa97fcf1378dd1f25f8c5f7b3f7c51
LhGomesss <luishfeitosa01@gmail.com>
2025-03-31 08:07:18 -0300

    Programa finalizado

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿string comR;
+string semR;
+Console.Write("Oi, eu sou o cebolinha, fale algo: ");
+comR = Console.ReadLine()!;
+semR=comR.Replace('r','l').Replace('R','L');
+
+Console.WriteLine($"Frase original:{comR}");
+Console.WriteLine();
+Console.WriteLine($"Flase do cebolinha:{semR}");
